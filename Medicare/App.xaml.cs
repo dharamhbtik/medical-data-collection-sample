3f046fab84f1b48769418b9027b734beb655fb09
Dharmendra Kumar <dharmendra.hbti@gmail.com>
2022-06-24 19:59:21 +0530

    Adding to VCS

---
+++ Medicare/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Medicare.Services;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Medicare
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<PatientService>();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Medicare/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Medicare.ViewModel;
+using Xamarin.Forms;
+
+namespace Medicare
+{
+    public partial class MainPage : ContentPage
+    {
+        PatientViewModel vm;
+        public MainPage()
+        {
+            InitializeComponent();
+            vm = new PatientViewModel();
+            this.BindingContext = vm;
+        }
+    }
+}
